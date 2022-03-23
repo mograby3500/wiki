77ccc35a9215803aecd503c6f675e528b5f06cdc
mograby3500 <mograby3500@gmail.com>
2022-03-23 10:04:58 +0200

    add entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entery(request, title):
+    f = util.get_entry(title)
+    if f == None:
+        entery_html = "<h1>Not Found</h1>"
+    else:
+        entery_html = markdown.markdown(f)
+    
+    return render(request, "encyclopedia/entery.html", {
+        "title" : title,
+        "entery" : entery_html
     })
 
